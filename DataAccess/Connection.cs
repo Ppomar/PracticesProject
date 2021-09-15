52a535e685a00f0189899cfc5833906627cb3c3c
Omar Quijano <pepeomarqc@hotmail.com>
2021-09-15 11:06:56 -0500

    OQ - Upload Project - Update

---
+++ DataAccess/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Connection
+    {
+        public static string StringConnection = ConfigurationManager.ConnectionStrings["Connection"].ToString();
+    }
+}
